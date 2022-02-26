820724210ddc8087772eb6d04d18744f3ea53cc2
LucasRD1978 <lrd1978@outlook.com>
2022-02-26 15:05:52 -0300

    Feat: creation of models from the database

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -3,10 +3,52 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
+
   sequelize.define('videogame', {
+
+    Id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false, 
+      primaryKey: true
+    },
+
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    release_date: {
+      type: DataTypes.STRING,
+    },
+
+    rating: {
+      type: DataTypes.FLOAT,
+    },
+
+    platform: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    createInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+
+  },
+  {
+    timestamps: false
   });
 };
